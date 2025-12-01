9dc33163b89677375b812af928073601315f4276
akaletekoffilevis <l.akalete20@gmail.com>
2025-12-02 00:34:26 +0100

    Times renommer en schedules

--- src/data/busAxe.js
+++ src/data/busAxe.js
@@ -4,7 +4,351 @@ export const busAxes = [
     name: "Axe Ny - Niamey 2000",
     shortName: "Ny",
     color: "#10B981",
-    times: [
+    schedules: [
+      [ 
+        "11:00", 
+        [1, 2, 3, 4]
+      ],
+      [
+        "12:30", 
+        [1, 2, 3, 4, 5, 6, 7]
+      ],
+      [
+        "16:30", 
+        [1, 2, 3, 4, 5, 6]
+      ],
+      [
+        "17:00",
+        [1, 2, 3, 4, 6, 7]
+      ],
+      [
+        "18:30",
+        [1, 2, 3, 4, 6, 7]
+      ],
+    ],
+    stops: [
+      {
+        id: 1,
+        name: "Station Royal",
+        description: "Arret No 1",
+        lat: 13.5137,
+        lng: 2.1098,
+        order: 1,
+      },
+      {
+        id: 2,
+        name: "Commisariat Niamey 2000",
+        description: "Arret No 2",
+        lat: 13.5115,
+        lng: 2.1125,
+        order: 2,
+      },
+      {
+        id: 3,
+        name: "Boulangerie Youssra",
+        description: "Arret No 3",
+        lat: 13.5116,
+        lng: 2.1191,
+        order: 3,
+      },
+        {
+        id: 4,
+        name: "Station Petroba",
+        description: "Arret No 4",
+        lat: 13.5416,
+        lng: 3.1191,
+        order: 4,
+      },
+        {
+        id: 5,
+        name: "Station RPS",
+        description: "Arret No 5",
+        lat: 14.5116,
+        lng: 4.1891,
+        order: 5,
+      },
+        {
+        id: 6,
+        name: "Unknown",
+        description: "Arret No 6",
+        lat: 20.5116,
+        lng: 6.1891,
+        order: 6,
+      },
+        {
+        id: 7,
+        name: "Station Morey",
+        description: "Arret No 7",
+        lat: 23.5116,
+        lng: 4.1191,
+        order: 7,
+      },
+    ]
+  },
+   {
+    id: 2,
+    name: "Axe Ny - Niamey 2000",
+    shortName: "Ny",
+    color: "#10B981",
+    schedules: [
+      [ 
+        "11:00", 
+        [1, 2, 3, 4]
+      ],
+      [
+        "12:30", 
+        [1, 2, 3, 4, 5, 6, 7]
+      ],
+      [
+        "16:30", 
+        [1, 2, 3, 4, 5, 6]
+      ],
+      [
+        "17:00",
+        [1, 2, 3, 4, 6, 7]
+      ],
+      [
+        "18:30",
+        [1, 2, 3, 4, 6, 7]
+      ],
+    ],
+    stops: [
+      {
+        id: 1,
+        name: "Station Royal",
+        description: "Arret No 1",
+        lat: 13.5137,
+        lng: 2.1098,
+        order: 1,
+      },
+      {
+        id: 2,
+        name: "Commisariat Niamey 2000",
+        description: "Arret No 2",
+        lat: 13.5115,
+        lng: 2.1125,
+        order: 2,
+      },
+      {
+        id: 3,
+        name: "Boulangerie Youssra",
+        description: "Arret No 3",
+        lat: 13.5116,
+        lng: 2.1191,
+        order: 3,
+      },
+        {
+        id: 4,
+        name: "Station Petroba",
+        description: "Arret No 4",
+        lat: 13.5416,
+        lng: 3.1191,
+        order: 4,
+      },
+        {
+        id: 5,
+        name: "Station RPS",
+        description: "Arret No 5",
+        lat: 14.5116,
+        lng: 4.1891,
+        order: 5,
+      },
+        {
+        id: 6,
+        name: "Unknown",
+        description: "Arret No 6",
+        lat: 20.5116,
+        lng: 6.1891,
+        order: 6,
+      },
+        {
+        id: 7,
+        name: "Station Morey",
+        description: "Arret No 7",
+        lat: 23.5116,
+        lng: 4.1191,
+        order: 7,
+      },
+    ]
+  },
+   {
+    id:3,
+    name: "Axe Ny - Niamey 2000",
+    shortName: "Ny",
+    color: "#10B981",
+    schedules: [
+      [ 
+        "11:00", 
+        [1, 2, 3, 4]
+      ],
+      [
+        "12:30", 
+        [1, 2, 3, 4, 5, 6, 7]
+      ],
+      [
+        "16:30", 
+        [1, 2, 3, 4, 5, 6]
+      ],
+      [
+        "17:00",
+        [1, 2, 3, 4, 6, 7]
+      ],
+      [
+        "18:30",
+        [1, 2, 3, 4, 6, 7]
+      ],
+    ],
+    stops: [
+      {
+        id: 1,
+        name: "Station Royal",
+        description: "Arret No 1",
+        lat: 13.5137,
+        lng: 2.1098,
+        order: 1,
+      },
+      {
+        id: 2,
+        name: "Commisariat Niamey 2000",
+        description: "Arret No 2",
+        lat: 13.5115,
+        lng: 2.1125,
+        order: 2,
+      },
+      {
+        id: 3,
+        name: "Boulangerie Youssra",
+        description: "Arret No 3",
+        lat: 13.5116,
+        lng: 2.1191,
+        order: 3,
+      },
+        {
+        id: 4,
+        name: "Station Petroba",
+        description: "Arret No 4",
+        lat: 13.5416,
+        lng: 3.1191,
+        order: 4,
+      },
+        {
+        id: 5,
+        name: "Station RPS",
+        description: "Arret No 5",
+        lat: 14.5116,
+        lng: 4.1891,
+        order: 5,
+      },
+        {
+        id: 6,
+        name: "Unknown",
+        description: "Arret No 6",
+        lat: 20.5116,
+        lng: 6.1891,
+        order: 6,
+      },
+        {
+        id: 7,
+        name: "Station Morey",
+        description: "Arret No 7",
+        lat: 23.5116,
+        lng: 4.1191,
+        order: 7,
+      },
+    ]
+  },
+   {
+    id: 4,
+    name: "Axe Ny - Niamey 2000",
+    shortName: "Ny",
+    color: "#10B981",
+    schedules: [
+      [ 
+        "11:00", 
+        [1, 2, 3, 4]
+      ],
+      [
+        "12:30", 
+        [1, 2, 3, 4, 5, 6, 7]
+      ],
+      [
+        "16:30", 
+        [1, 2, 3, 4, 5, 6]
+      ],
+      [
+        "17:00",
+        [1, 2, 3, 4, 6, 7]
+      ],
+      [
+        "18:30",
+        [1, 2, 3, 4, 6, 7]
+      ],
+    ],
+    stops: [
+      {
+        id: 1,
+        name: "Station Royal",
+        description: "Arret No 1",
+        lat: 13.5137,
+        lng: 2.1098,
+        order: 1,
+      },
+      {
+        id: 2,
+        name: "Commisariat Niamey 2000",
+        description: "Arret No 2",
+        lat: 13.5115,
+        lng: 2.1125,
+        order: 2,
+      },
+      {
+        id: 3,
+        name: "Boulangerie Youssra",
+        description: "Arret No 3",
+        lat: 13.5116,
+        lng: 2.1191,
+        order: 3,
+      },
+        {
+        id: 4,
+        name: "Station Petroba",
+        description: "Arret No 4",
+        lat: 13.5416,
+        lng: 3.1191,
+        order: 4,
+      },
+        {
+        id: 5,
+        name: "Station RPS",
+        description: "Arret No 5",
+        lat: 14.5116,
+        lng: 4.1891,
+        order: 5,
+      },
+        {
+        id: 6,
+        name: "Unknown",
+        description: "Arret No 6",
+        lat: 20.5116,
+        lng: 6.1891,
+        order: 6,
+      },
+        {
+        id: 7,
+        name: "Station Morey",
+        description: "Arret No 7",
+        lat: 23.5116,
+        lng: 4.1191,
+        order: 7,
+      },
+    ]
+  },
+   {
+    id: 5,
+    name: "Axe Ny - Niamey 2000",
+    shortName: "Ny",
+    color: "#10B981",
+    schedules: [
       [ 
         "11:00", 
         [1, 2, 3, 4]
