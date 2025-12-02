8feecedd0669536bcf069fb39ff3644a4c7887b3
ABDOUL RAOUF <abdoulraoufharouna62@gmail.com>
2025-12-02 14:35:57 +0100

    ajout de la troisiéme page

--- src/components/Trajectory/TrajectoryMap.js
+++ src/components/Trajectory/TrajectoryMap.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { View } from 'react-native'
 
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
-      return(
-            <View>
-            < MapView
-                  initialRegion={{
-                        latitude: 37.78825,
-                        longitude: - 122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                  }}
-            />
-      </View>
-      ) 
+      return (
+            <View >
+                  <MapView
+                        
+                        initialRegion={{
+                              latitude: 13.5116,
+                              longitude: 2.1254,
+                              latitudeDelta: 0.05,
+                              longitudeDelta: 0.05,
+                        }}
+                  >
+                        <Marker
+                              coordinate={{ latitude: 13.5116, longitude: 2.1254 }}
+                              title="Niamey"
+                              description="Gare de bus"
+                        />
+                  </MapView>
+            </View>
+
+      )
 }
 export default Map
--- src/screens/TrajetScreen.js
+++ src/screens/TrajetScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Map from "../components/Trajectory/TrajectoryMap"
+// import Map from "../components/Trajectory/TrajectoryMap"
 import { View,Text } from "react-native";
+import { MapView } from "@maplibre/maplibre-react-native"
+// import MapView, { Marker, PROVIDER_GOOGLE  } from 'react-native-maps';
 
 const Trajet = () =>{
       return(
             <View>
                   <Text>le Trajet du bus sur l'axe</Text>
-                  <Map/>
+                  <MapView style={{ flex: 1 }} />    
             </View>
       )
 }
