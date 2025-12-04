7498b74e08ccd815042c851749e84c2617eb25bf
ABDOUL RAOUF <abdoulraoufharouna62@gmail.com>
2025-12-04 02:11:27 +0100

    RAS

--- src/screens/TrajetScreen.js
+++ src/screens/TrajetScreen.js
@@ -1,14 +1,12 @@
 import React from "react";
 // import Map from "../components/Trajectory/TrajectoryMap"
 import { View,Text } from "react-native";
-import { MapView } from "@maplibre/maplibre-react-native"
-// import MapView, { Marker, PROVIDER_GOOGLE  } from 'react-native-maps';
 
 const Trajet = () =>{
       return(
             <View>
                   <Text>le Trajet du bus sur l'axe</Text>
-                  <MapView style={{ flex: 1 }} />    
+                    
             </View>
       )
 }
